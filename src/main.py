861ee8650e2ca621de760a66eeda61fde625eb11
AlekWithK <38448418+AlekWithK@users.noreply.github.com>
2021-11-14 05:36:56 -0800

    added tabs, formatting

---
+++ src/main.py
@@ -0,0 +1,22 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.lang import Builder
+from kivy.uix.tabbedpanel import TabbedPanel
+from kivy.core.window import Window
+
+
+Window.clearcolor = (.21,.21,.21,1) #Button header color
+Builder.load_file('ui.kv')
+
+class Frame(TabbedPanel):
+        pass
+
+class ESOCalc(App):
+    def build(self):
+        self.title = 'ESO Calculators'
+        self.icon = 'Resources/eso.png'
+        return Frame()
+
+if __name__ == "__main__":
+    ESOCalc().run()
