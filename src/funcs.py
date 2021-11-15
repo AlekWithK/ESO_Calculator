782bb2c31996b81930daeac44f43819f4db3ad6b
AlekWithK <38448418+AlekWithK@users.noreply.github.com>
2021-11-15 05:43:54 -0800

    buttons/functions implemented

---
+++ src/funcs.py
@@ -0,0 +1,5 @@
+def normalizer(t_dps, c_chance, t_hits, t_c_hits, t_non_hits, c_damage):
+    
+
+    
+    return result
--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
+from kivy.config import Config
+Config.set('graphics', 'resizable', False)
+
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
@@ -5,19 +8,30 @@ from kivy.lang import Builder
 from kivy.uix.tabbedpanel import TabbedPanel
 from kivy.core.window import Window
 from kivy.uix.textinput import TextInput
+from kivy.clock import Clock
 
+from funcs import normalizer
 
+Window.size = (500, 500)
 Window.clearcolor = (.21,.21,.21,1) #Button header color
 Builder.load_file('ui.kv')
 
 class Frame(TabbedPanel):
+    def __init__(self, **kwargs):
+        super(Frame, self).__init__(**kwargs)
+    
+    def button_pressed(self):
+        t_dps = int(self.ids.t_dps.text)
+        self.ids.result_lbl.text = f'Result: {t_dps}'
+        self.ids.t_dps.text = ''        
         pass
 
 class ESOCalc(App):
     def build(self):
         self.title = 'ESO Calculators'
         self.icon = 'Resources/eso.png'
-        return Frame()
+        return Frame()   
+        
 
 if __name__ == "__main__":
     ESOCalc().run()
