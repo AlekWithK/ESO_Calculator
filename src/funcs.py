591b5c548a2a1261e146964662a543d1860b97ab
AlekWithK <38448418+AlekWithK@users.noreply.github.com>
2021-11-16 04:47:04 -0800

    Finished parse normalizer tab

--- src/funcs.py
+++ src/funcs.py
@@ -1,5 +1,19 @@
 def normalizer(t_dps, c_chance, t_hits, t_c_hits, t_non_hits, c_damage):
     
-
+    c_chance = c_chance / 100
+    c_damage = c_damage / 100
     
-    return result
+    #Total hits - hits that cannot crit -> Actual crit %
+    valid_hits = t_hits - t_non_hits
+    true_crit = t_c_hits / valid_hits
+    
+    #Calculating % difference
+    dps_delta = ((1 + (c_chance * c_damage)) / (1 + true_crit * c_damage) - 1)
+    dps_delta_str = round((dps_delta * 100), 2)
+    str_delta = f'+{dps_delta_str}%' if dps_delta >= 0 else f'{dps_delta_str}%'
+    
+    #Calculating new DPS total
+    result = round((t_dps * (1 + dps_delta)), 0)
+    str_result = str(result)[:-2]
+    
+    return str_result, str_delta
--- src/main.py
+++ src/main.py
@@ -13,7 +13,7 @@ from kivy.clock import Clock
 from funcs import normalizer
 
 Window.size = (500, 500)
-Window.clearcolor = (.21,.21,.21,1) #Button header color
+Window.clearcolor = (.21,.21,.21,0) #Button header color
 Builder.load_file('ui.kv')
 
 class Frame(TabbedPanel):
@@ -21,15 +21,22 @@ class Frame(TabbedPanel):
         super(Frame, self).__init__(**kwargs)
     
     def button_pressed(self):
-        t_dps = int(self.ids.t_dps.text)
-        self.ids.result_lbl.text = f'Result: {t_dps}'
-        self.ids.t_dps.text = ''        
-        pass
-
+        try:
+            t_dps = int(self.ids.t_dps.text)
+            c_chance = float(self.ids.c_chance.text)
+            t_hits = int(self.ids.t_hits.text)
+            t_c_hits = int(self.ids.t_c_hits.text)
+            t_non_hits = int(self.ids.t_non_hits.text)
+            c_damage = float(self.ids.c_damage.text)
+            
+            result, delta = normalizer(t_dps, c_chance, t_hits, t_c_hits, t_non_hits, c_damage)
+            self.ids.result_lbl.text = f"""Result: {result}\nChange: {delta}"""
+        except:
+            self.ids.result_lbl.text = f'Please check \n your inputs!'      
 class ESOCalc(App):
     def build(self):
         self.title = 'ESO Calculators'
-        self.icon = 'Resources/eso.png'
+        self.icon = 'Resources/icon.png'
         return Frame()   
         
 
